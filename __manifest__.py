7bf7d91a6e98bb53d915581a0f36cd72df309126
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-06 21:16:10 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,15 @@
     "description": "Imprime etiquetas por unidad comprada desde órdenes de compra.",
     "category": "Purchases",
     "author": "TuNombre",
+    "license": "LGPL-3",
     "depends": ["purchase", "stock"],
     "data": [
-    "security/ir.model.access.csv",
-    "views/pucharse_label_wizard_view.xml",
-    "views/pucharse_order_view.xml",
-],
+        "security/ir.model.access.csv",
+        "views/pucharse_label_wizard_view.xml",
+        "views/pucharse_order_view.xml",
+        "reports/report.xml",
+        "reports/report_purchase_label_template.xml"
+    ],
     "installable": True,
     "application": False
 }
